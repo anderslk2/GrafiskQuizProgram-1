aa89b305877fa1f697dc251505ac0c7ba4c2cda8
MathiasK <MathiasK>
2015-12-25 19:37:09 +0100

    test ændringer til github

---
+++ Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApplication1
+{
+//TODO Fjern denne : test kommentar til at illustrere om GitHub virker
+    class Datahandler
+    {
+        int maxsymptomantal;
+        public XElement doc;
+        public string sygdom;
+        public List<string> kandidatsymptomer;
+        public List<string> korrektesymptomer;
+        private Random rnd;
+
+        public Datahandler(int antal)
+        {
+            maxsymptomantal = antal;
+        }
+
+        public void indlæsdata(string stinavn)
+        {
+            rnd = new Random();
+            doc = XElement.Load(stinavn);
+        }
+
+        public void VælgSygdomAtSpørgeOm()
+        {
+            sygdom = doc.Elements("sygdom").OrderBy(elt => rnd.Next()).ToList()[0].Attribute("navn").Value;
+
+            korrektesymptomer = doc.Elements("sygdom").Where(asymp => asymp.Attribute("navn").Value == sygdom).ToList().SelectMany(syg => syg.Elements("symptom").ToList(), (a, b) => b.Attribute("navn").Value).ToList();
+            kandidatsymptomer = GenererListeAfkandidatsymptomer();
+        }
+
+        List<string> GenererListeAfkandidatsymptomer()
+        {
+            List<string> AlleSymptomer = doc.Elements("sygdom").ToList<XElement>().SelectMany(syg => syg.Elements("symptom").ToList(), (a, b) => b.Attribute("navn").Value).ToList();
+
+            var SymptomerIkkeFordenneSygdom = AlleSymptomer.Except(korrektesymptomer).OrderBy(sfds => rnd.Next()); ;
+
+            var maxantalsymptomerfralister = Math.Min((decimal)SymptomerIkkeFordenneSygdom.Count(), (decimal)korrektesymptomer.Count());
+            List<string> displaysymptomer = new List<string>() { korrektesymptomer.ToList()[0] };
+
+            for (int i = 1; i < Math.Min((decimal)maxsymptomantal, (decimal)maxantalsymptomerfralister); i++)
+            {
+                if (rnd.Next(2) == 0)
+                    displaysymptomer.Add(korrektesymptomer.ToList()[i]);
+                else
+                    displaysymptomer.Add(SymptomerIkkeFordenneSygdom.ToList()[i]);
+            }
+            displaysymptomer = displaysymptomer.OrderBy(ds => rnd.Next(1000)).ToList();
+
+            return displaysymptomer;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ConsoleApplication1;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        static int maxsympantal = 4;
+        Datahandler datahandler;
+        public Form1()
+        {
+            datahandler = new Datahandler(maxsympantal);
+            datahandler.indlæsdata(@"C:\Users\Anders\Downloads\perlin_noise_csharp\QuestionProgram\QuestionProgram\sygdomme.xml");
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.BackColor = SystemColors.Control;
+
+            button3.BackColor = SystemColors.Control;
+
+            button4.BackColor = SystemColors.Control;
+
+            button5.BackColor = SystemColors.Control;
+
+
+            datahandler.VælgSygdomAtSpørgeOm();
+
+            spørgsmålsviser.Text = AskQuestion(datahandler.sygdom, datahandler.kandidatsymptomer);
+
+            button2.Text = datahandler.kandidatsymptomer[0];
+
+            button3.Text = datahandler.kandidatsymptomer[1];
+
+            button4.Text = datahandler.kandidatsymptomer[2];
+
+            button5.Text = datahandler.kandidatsymptomer[3];
+        }
+
+        private string AskQuestion(string sygdom, List<string> kandidatsymptomer)
+        {
+            return string.Format("For sygdommen {0} gives følgende kandidatsymptomer. Skriv et rigtigt symptom.", sygdom);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(button2.ForeColor==SystemColors.ControlText)
+            button2.ForeColor = Color.LightBlue;
+            else
+                button2.ForeColor = SystemColors.ControlText;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            List<Button> buttonlist = new List<Button>() { button2, button3, button4, button5 };
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (!buttonlist[i].Enabled && datahandler.korrektesymptomer.Contains(datahandler.kandidatsymptomer[i]))
+                {
+                    buttonlist[i].BackColor = Color.LightGreen;
+                    buttonlist[i].Enabled = true;
+                    
+                }
+                else if (buttonlist[i].Enabled && !datahandler.korrektesymptomer.Contains(datahandler.kandidatsymptomer[i]))
+                {
+                    buttonlist[i].BackColor = Color.LightGreen;
+                    buttonlist[i].Enabled = true;
+                }
+                else
+                    buttonlist[i].BackColor = Color.Red;
+                    buttonlist[i].Enabled = true;
+            }
+        }
+    }
+}
